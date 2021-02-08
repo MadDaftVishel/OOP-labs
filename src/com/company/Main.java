e96ddbc24224a97bdee8d8b4a42d6e1be52b1014
maddaftvishel <madvishel@gmail.com>
2021-02-08 10:57:08 +0200

    lab1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,29 @@
 package com.company;
 
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-	System.out.println("wha-a...?");
+        double s = 0, m, n;
+        int a = 1, b = 1;
+        final int c = 2;
+        System.out.println("Print a,m");
+        Scanner m_scaner=new Scanner(System.in);
+        m = m_scaner.nextDouble();
+        Scanner n_scaner=new Scanner(System.in);
+        n = n_scaner.nextDouble();
+        n_scaner.close();
+        m_scaner.close();
+        for (double i = a; i <= n; i++) {
+            for (double j = b; j <= m; j++) {
+                s = (i / j) / (i - c);
+                }
+            }
+        if (n >= c) {
+            System.out.println("'i' cannot equal 'c' (a/0)");
+    } else{
+            System.out.println(s);
+        }
     }
 }
